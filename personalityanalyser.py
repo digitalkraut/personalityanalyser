4748223b84810e93e3b081d6d2d1d8e0a688f594
digitalkraut <michael@digitalkraut.de>
2013-07-12 10:11:36 +0200

    Create personalityanalyser.py
    First upload of extraordinary personality analyser

---
+++ personalityanalyser.py
@@ -0,0 +1,14 @@
+#personality analyser to find out what kind of human you are
+
+Startabfrage = "nein"
+
+print ("================================================================")
+print ("Ich bin ihr hochwissenschaftlicher Persönlichkeits Analaysator")
+print ("================================================================")
+
+Startabfrage = input("Können wir starten?")
+
+        if Startabfrage = "ja":
+                print ("Das ist aber fein")
+        else:
+                print ("Schade!")
